5576706d34db35ad31c25ea128342a2545381240
b5710545015 <chawakorn.a@ku.th>
2016-06-03 22:28:04 +0700

    first commit

---
+++ app/src/main/java/th/aku/chawakorn/diaryapp/NewPageActivity.java
@@ -0,0 +1,13 @@
+package th.aku.chawakorn.diaryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_page);
+    }
+}
